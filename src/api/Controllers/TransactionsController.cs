4397df3a93f5dc2f028da7c99d972ad91e8cf8f7
valdirmoreira <valdir.moreira.junior@gmailcom>
2018-07-28 22:42:14 -0300

    #1 Criação do controller de transações

---
+++ src/api/Controllers/TransactionsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/v2/transactions")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+    }
+}
