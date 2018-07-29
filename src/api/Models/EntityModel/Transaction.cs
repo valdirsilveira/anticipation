e07bf72ad2d6a4d20f912f011feb877332cdeee8
valdirmoreira <valdir.moreira.junior@gmailcom>
2018-07-28 22:23:00 -0300

    Criação dos entities models de Transações

---
+++ src/api/Models/EntityModel/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace api.Models.EntityModel
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public bool? AcquirerConfirmation { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int Installments { get; set; }
+        public DateTime? TransferDate { get; set; }        
+        public decimal TransactionAmount { get; set; }
+        public decimal? TransferAmount { get; set; }        
+    }
+}
---
+++ src/api/Models/EntityModel/TransactionQuery.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace api.Models.EntityModel
+{
+    public static class TransactionQuery
+    {
+        #region OrderByDescendingCreatedAt
+        public static IQueryable<Transaction> OrderByDescendingCreatedAt(this IQueryable<Transaction> transactions)
+        {
+            return transactions.OrderByDescending(transaction => transaction.CreatedAt);
+        }
+        #endregion
+
+        #region WhereDateFrom
+        public static IQueryable<Transaction> WhereDateFrom(this IQueryable<Transaction> transactions, DateTime startDate)
+        {
+            return transactions.Where(transaction => transaction.CreatedAt.Date >= startDate.Date);
+        }
+        #endregion
+
+        #region WhereAvailableAnticipation
+        public static IQueryable<Transaction> WhereAvailableAnticipation(this IQueryable<Transaction> transactions)
+        {
+            return transactions.Where(transaction =>
+                transaction.AcquirerConfirmation == true
+                && transaction.TransferDate == null
+                && transaction.TransferAmount == null);
+        }
+        #endregion
+
+        #region WhereDateUntil
+        public static IQueryable<Transaction> WhereDateUntil(this IQueryable<Transaction> transactions, DateTime endDate)
+        {
+            return transactions.Where(transaction => transaction.CreatedAt.Date <= endDate.Date);
+        }
+        #endregion
+    }
+}
