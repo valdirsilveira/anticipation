01a9609b2c914dfc28e129722e31cf8b973f5484
valdirmoreira <valdir.moreira.junior@gmailcom>
2018-07-28 22:57:12 -0300

    #1 - Adiciona contexto padrão e migrações iniciais

---
+++ src/api/Migrations/20180729015421_Initial.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AntecipacaoStatus",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Descricao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AntecipacaoStatus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transacoes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ConfirmacaoAdquirente = table.Column<bool>(nullable: true),
+                    DataTransacao = table.Column<DateTime>(nullable: false, defaultValueSql: "getdate()"),
+                    parcelas = table.Column<int>(nullable: false),
+                    DataRepasse = table.Column<DateTime>(nullable: true),
+                    ValorTransacao = table.Column<decimal>(type: "decimal(8,2)", nullable: false),
+                    ValorRepasse = table.Column<decimal>(type: "decimal(8,2)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_transacoes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Antecipacoes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DataAnalise = table.Column<DateTime>(nullable: true),
+                    AntecipacaoStatusId = table.Column<long>(nullable: false),
+                    ResultadoAnalise = table.Column<bool>(nullable: true),
+                    DataAntecipacao = table.Column<DateTime>(nullable: false, defaultValueSql: "getdate()"),
+                    ValorSolicitado = table.Column<decimal>(type: "decimal(8,2)", nullable: false),
+                    ValorRepasse = table.Column<decimal>(type: "decimal(8,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Antecipacoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Antecipacoes_AntecipacaoStatus_AntecipacaoStatusId",
+                        column: x => x.AntecipacaoStatusId,
+                        principalTable: "AntecipacaoStatus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Antecipacoes_AntecipacaoStatusId",
+                table: "Antecipacoes",
+                column: "AntecipacaoStatusId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Antecipacoes");
+
+            migrationBuilder.DropTable(
+                name: "transacoes");
+
+            migrationBuilder.DropTable(
+                name: "AntecipacaoStatus");
+        }
+    }
+}
--- src/api/Startup.cs
+++ src/api/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using api.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace api
 {
@@ -26,6 +21,8 @@ namespace api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<ApiDbContext>(options =>
+                            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
