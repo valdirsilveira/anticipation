654b7eda8915e0b5b88923db4929ea78c2c6eb6f
valdirmoreira <valdir.moreira.junior@gmailcom>
2018-07-29 17:12:09 -0300

    Adiciona seed de transações e status de antecipações para facilitar o desenvolvimento

---
+++ src/api/Infrastructure/ApiDbSeed.cs
@@ -0,0 +1,54 @@
+﻿using api.Models.EntityModel;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace api.Infrastructure
+{
+    public static class ApiDbSeed
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<ApiDbContext>();
+            context.Database.EnsureCreated();
+
+            #region AnticipationStatus
+            if (!context.AnticipationStatus.Any())
+            {
+                context.AnticipationStatus.Add(new AnticipationStatus() { Description = "Aguardando análise" });
+                context.AnticipationStatus.Add(new AnticipationStatus() { Description = "Em análise" });
+                context.AnticipationStatus.Add(new AnticipationStatus() { Description = "Finalizada" });
+                context.SaveChanges();
+            }
+            #endregion
+
+            #region Transactions
+            if (!context.Transactions.Any())
+            {
+                context.Transactions.Add(new Transaction()
+                {
+                    AcquirerConfirmation = true,
+                    CreatedAt = DateTime.Now,
+                    Installments = 1,
+                    TransactionAmount = (decimal)250.00
+                });
+                context.Transactions.Add(new Transaction()
+                {
+                    AcquirerConfirmation = true,
+                    CreatedAt = DateTime.Now,
+                    Installments = 1,
+                    TransactionAmount = (decimal)300
+                });
+                context.Transactions.Add(new Transaction()
+                {
+                    AcquirerConfirmation = true,
+                    CreatedAt = DateTime.Now,
+                    Installments = 2,
+                    TransactionAmount = (decimal)500
+                });
+                context.SaveChanges();
+            }         
+            #endregion
+        }
+    }
+}
--- src/api/Program.cs
+++ src/api/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Infrastructure;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace api
@@ -14,7 +16,24 @@ namespace api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApiDbContext>();
+                try
+                {
+                    ApiDbSeed.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
