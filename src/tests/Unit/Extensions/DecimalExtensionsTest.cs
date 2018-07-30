2c37a179d11101376f14880a5f88c94aa7e76a11
valdirmoreira <valdir.junior.aju@gmail.com>
2018-07-30 16:20:48 -0300

    Criação do projeto de testes de unidade

---
+++ src/tests/Unit/Extensions/DecimalExtensionsTest.cs
@@ -0,0 +1,19 @@
+﻿using api.Extensions;
+using System;
+using Xunit;
+
+namespace Unit.Extensions
+{
+    public class DecimalExtensionsTest
+    {
+        [Fact]
+        public void CalculateTransactionFeesTest()
+        {
+            var transactionAmount = (decimal) 200;
+            var installments = 1;
+            var expectedResult = (decimal)192.40;
+            var transferAmount = transactionAmount.CalculateTransactionFees(installments);
+            Assert.Equal(transferAmount, expectedResult);
+        }
+    }
+}
